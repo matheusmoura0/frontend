83afa337ec9ead4c4c96988f50dd0fbd6d3c93c2
Matheus <matheusmoura2110@gmail.com>
2022-06-30 23:19:50 -0300

    feat: todoCard logic

---
+++ frontend/src/components/TodoCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useState } from "react";
+import { completeTodo } from '../helpers/requests';
+
+const TodoCard = ({ todo, deleteTodo, updateTodo, editTodo }) => { 
+    const [isEditing, setIsEditing] = useState(false);
+    const [todoText, setTodoText] = useState(todo.todo);
+
+    const handleEdit = () => { 
+        setIsEditing(true);
+        input.current.focus();
+    }
+    const stopEditing = () => { 
+        setIsEditing(false);
+        setTodoText(todo.todo);
+    }
+
+    const completeTodo = async () => {
+        await completeTodo(todo.id);
+    };
+
+    return (
+        <div className={`todo ${todo.completed ? 'todo-completed' : ''}`}>
+            <input type="checkbox"  checked={todo.completed} onChange={ completeTodo } />
+            <input type="text" ref={ input } value={todoText} readOnly={!isEditing} 
+            onChange={(e) => setTodoText(e.target.value)}
+            />
+            <div className="todo__controls">
+            {!isEditing ? (
+            <>
+            {!todo.completed && <button onClick={handleEdit}>Edit</button>}
+            <button onClick={deleteTodo}>Delete</button>
+            </>
+        ) : (
+            <>
+            <button onClick={stopEditing}>Cancel</button>
+            <button onClick={() => editTodo(todo.id, todoText, setIsEditing(false))}>Save</button>
+            </>
+        )}
+            </div>
+        </div>
+    );
+}
+
+export default TodoCard;
