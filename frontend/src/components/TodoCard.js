6ddc1aa648cf9c6677b8efbc4ac9de980cb6335f
Matheus <matheusmoura2110@gmail.com>
2022-07-01 14:48:06 -0300

    fix: Todo component

--- frontend/src/components/TodoCard.js
+++ frontend/src/components/TodoCard.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { useState } from "react";
-import { completeTodoApi } from '../helpers/requests';
 
-const TodoCard = ({ todo, deleteTodo, updateTodo, editTodo }) => { 
-    const [isEditing, setIsEditing] = useState(false);
+const TodoCard = ({ todo, deleteTodo, completeTodo, editTodo }) => { 
     const input = React.useRef(null);
-    const [todoText, setTodoText] = useState(todo.todo);
+    const [edit, setEdit] = useState(false);
+    const [content, setContent] = useState(todo.todo);
 
-    const handleEdit = () => { 
-        setIsEditing(true);
+    const handleEdit = e => { 
+        setEdit(true);
 
         input.current.focus();
-    }
-    const stopEditing = () => { 
-        setIsEditing(false);
-        setTodoText(todo.todo);
+
     }
 
-    const completeTodos = async () => {
-        await completeTodoApi(todo.id);
-    };
+    const stopEditing = e => { 
+        setEdit(false);
+        setContent(todo.todo);
+    }
+    const completed = async () => { 
+        await completeTodo(todo.id);
+    }
 
+    
     return (
         <div className={`todo ${todo.completed ? 'todo-completed' : ''}`}>
-            <input type="checkbox"  checked={todo.completed} onChange={ completeTodos } />
-            <input type="text" ref={ input } value={todoText} readOnly={!isEditing} 
-            onChange={(e) => setTodoText(e.target.value)}
+            <input type="checkbox"  checked={todo.completed} onChange={ completed } />
+            <input type="text" ref={ input } value={content} readOnly={!edit} 
+            onChange={(e) => setContent(e.target.value)}
             />
             <div className="todo__controls">
-            {!isEditing ? (
+            {!edit ? (
             <>
             {!todo.completed && <button onClick={handleEdit}>Edit</button>}
             <button onClick={deleteTodo}>Delete</button>
@@ -36,7 +37,7 @@ const TodoCard = ({ todo, deleteTodo, updateTodo, editTodo }) => {
         ) : (
             <>
             <button onClick={stopEditing}>Cancel</button>
-            <button onClick={() => editTodo(todo.id, todoText, setIsEditing(false))}>Save</button>
+            <button onClick={() => editTodo(todo.id, content, setEdit(false))}>Save</button>
             </>
         )}
             </div>
@@ -45,3 +46,4 @@ const TodoCard = ({ todo, deleteTodo, updateTodo, editTodo }) => {
 }
 
 export default TodoCard;
+
