e76b36245ad644aac5230dd2a072c2f976df11b8
Matheus <matheusmoura2110@gmail.com>
2022-07-03 13:53:07 -0300

    add: procfile to deploy

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PWD,
-        database: 'Todo_database',
+        database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'mysql',
