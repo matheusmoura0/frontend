bd1e12824e114817fb25653c06653ff84eb75c01
Matheus <matheusmoura2110@gmail.com>
2022-07-03 14:10:05 -0300

    heroku confg

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,14 @@ const cors = require('cors')
 
 const app = express();
 const todoRoute = require('./routes/index');
-const PORT = 3300 || processs.env.PORT;
+const PORT = processs.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json());
 app.use('/api/todos', todoRoute);
 
+app.get('/', (_req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT));
