719e6aba12970d7c8f4ab1b2611095aed88ccbe1
Matheus <matheusmoura2110@gmail.com>
2022-07-03 16:40:56 -0300

    test: att test file

--- backend/tests/todo-test.test.js
+++ backend/tests/todo-test.test.js
@@ -1,12 +1,27 @@
 const frisby = require('frisby');
-// const shell = require('shelljs');
+frisby.globalSetup({
+    timeout: 10000
+  });
+ const shell = require('shelljs');
 
 const api = 'http://localhost:3300';
 
+describe('1- can sign a new todo and return others', () => { 
 it('should create a todo', () => {
-  return frisby.post(`${api}/api/todos`, {
+    return frisby.post(`${api}/api/todos`, {
     todo: 'Test',
-  })
-  .expect('status', 201)
-  .expect('json', 'todo', 'Test');
+    })
+    .expect('status', 201)
+    .expect('json', 'todo', 'Test')
+    
+}).timeout(60000)
+
+it('should return a list of todos', () => {
+    return frisby.get(`${api}/api/todos`)
+    .expect('status', 200)
+});
+it('should return a todo by id', () => { 
+    return frisby.get(`${api}/api/todos/1`)
+    .expect('status', 200)
+})
 });
